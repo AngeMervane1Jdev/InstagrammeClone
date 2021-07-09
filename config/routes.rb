ea5d8fc31a7131b9fc91b44f1595a555fdc32c04
Ange Noutchogbe <onpasse1@gmail.com>
2021-07-09 02:06:56 +0000

    Registration and Connexion are possible with session

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new,:create, :show]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
